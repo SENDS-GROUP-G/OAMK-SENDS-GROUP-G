31c97c1bfb18b9d08150e9f04b093640b45b6efa
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-04-03 14:37:21 +0300

    Update styles.css and index.html, add static login and signup page (without logic)

--- js/domElements.js
+++
@@ -1,3 +0,0 @@
-export const postInput = document.getElementById('postInput');
-export const addPostBtn = document.getElementById('addPostBtn');
-export const postsContainer = document.getElementById('postsContainer');

7f2bfd078e908049a5c83cde73ca770a30449571
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-04-08 12:20:43 +0300

    Refactor codebase to Object-Oriented Programming approach and integrate Bootstrap framework

---
+++ js/classes/API.js
@@ -0,0 +1,62 @@
+export default class API {
+    static backendUrl = "http://localhost:3001";
+    static async sendRequest(url, method = "GET", body = null) {
+      const options = {
+        method,
+        headers: { "Content-Type": "application/json" },
+      };
+      if (body) options.body = JSON.stringify(body);
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+      return response.json();
+    }
+    static fetchPosts() {
+      return this.sendRequest(`${this.backendUrl}/`);
+    }
+    static fetchComments(postId) {
+      return this.sendRequest(`${this.backendUrl}/posts/${postId}/comments`);
+    }
+    static editComment(postId, commentId, newContent) {
+      return this.sendRequest(
+        `${this.backendUrl}/posts/${postId}/comments/${commentId}`,
+        "PUT",
+        {
+          user_id: 2,
+          comment_content: newContent,
+        }
+      );
+    }
+    static editPost(postId, newTitle, newContent) {
+      return this.sendRequest(`${this.backendUrl}/posts/${postId}`, "PUT", {
+        user_id: 1,
+        title: newTitle,
+        post_content: newContent,
+      });
+    }
+    static addPost(title, postContent) {
+      return this.sendRequest(`${this.backendUrl}/posts`, "POST", {
+        user_id: 2,
+        title,
+        post_content: postContent,
+      });
+    }
+    static deleteComment(postId, commentId) {
+      return this.sendRequest(
+        `${this.backendUrl}/posts/${postId}/comments/${commentId}`,
+        "DELETE"
+      );
+    }
+    static deletePost(postId) {
+      return this.sendRequest(`${this.backendUrl}/posts/${postId}`, "DELETE");
+    }
+    static addComment(postId, commentContent) {
+      return this.sendRequest(
+        `${this.backendUrl}/posts/${postId}/comments`,
+        "POST",
+        {
+          user_id: 1,
+          comment_content: commentContent,
+        }
+      );
+    }
+  }
---
+++ js/classes/User.js
@@ -0,0 +1,67 @@
+const BACKEND_URL = 'http://localhost:3001'
+
+class User {
+  #id = undefined
+  #email = undefined
+
+  constructor() {
+    const userFromStorage = sessionStorage.getItem('user')
+    if (userFromStorage) {
+      const userObject = JSON.parse(userFromStorage)
+      this.#id = userObject.id
+      this.#email = userObject.email
+    }
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get email() {
+    return this.#email
+  }
+
+  get isLoggedIn() {
+    return this.#id !== undefined ? true : false
+  }
+
+  async login(email,password) {
+    const data = JSON.stringify({email: email,password: password})
+    const response = await fetch(BACKEND_URL + '/user/login',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: data
+    })
+    if (response.ok === true) {
+      const json = await response.json()
+      this.#id = json.id
+      this.#email = json.email
+      sessionStorage.setItem('user',JSON.stringify(json))
+      return this
+    } else {
+      throw response.statusText
+    }
+  }
+
+  async register(user_name, email, password) {
+    const data = JSON.stringify({ user_name, email, password });
+    const response = await fetch(BACKEND_URL + '/register', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: data
+    });
+    if (response.ok === true) {
+      const json = await response.json();
+      this.#id = json.user_id;
+      this.#email = json.email;
+      sessionStorage.setItem('user', JSON.stringify(json));
+      return this;
+    } else {
+      throw response.statusText;
+    }
+  }
+
+}
+
+export { User }
+
